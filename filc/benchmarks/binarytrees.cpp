8bad8eb367a12c2587763f36e09a530b199c9242
Fil Pizlo <pizlo@mac.com>
2025-03-19 18:08:24 -0700

    Switch to the version of binarytrees that just relies on the GC.

--- filc/benchmarks/binarytrees.cpp
+++ filc/benchmarks/binarytrees.cpp
@@ -15,10 +15,7 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
-#include <memory_resource>
 #include <thread>
-
-using MemoryPool = std::pmr::monotonic_buffer_resource;
 
 struct Node
 {
@@ -33,13 +30,13 @@ struct Node
     }
 };
 
-Node* make(const int d, MemoryPool& store)
+Node* make(const int d)
 {
-    Node* root = static_cast<Node*>(store.allocate(sizeof(Node)));
+    Node* root = new Node;
     if (d > 0)
     {
-        root->l = make(d - 1, store);
-        root->r = make(d - 1, store);
+        root->l = make(d - 1);
+        root->r = make(d - 1);
     }
     else
     {
@@ -58,14 +55,11 @@ int run_parallel(unsigned depth, int iterations, unsigned int workers = std::thr
 
     for(unsigned i = 0; i < workers; ++i) {
         threads.push_back(std::thread([&counter, depth, &output] {
-            std::pmr::unsynchronized_pool_resource upperPool;
-            MemoryPool pool {&upperPool};
             int checksum = 0;
 
             while(--counter >= 0) {
-                Node* a     = make(depth, pool);
+                Node* a     = make(depth);
                 checksum    += a->check();
-                pool.release();
             }
 
             output += checksum;
@@ -88,15 +82,12 @@ int main(int argc, char* argv[])
 
     // Alloc then dealloc stretchdepth tree.
     {
-        MemoryPool store;
-
-        Node* c = make(stretch_depth, store);
+        Node* c = make(stretch_depth);
         std::cout << "stretch tree of depth " << stretch_depth << "\t "
                   << "check: " << c->check() << std::endl;
     }
 
-    MemoryPool long_lived_store;
-    Node* long_lived_tree = make(max_depth, long_lived_store);
+    Node* long_lived_tree = make(max_depth);
 
     for (int d = MIN_DEPTH; d <= max_depth; d += 2)
     {
