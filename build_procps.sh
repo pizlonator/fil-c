aef6207bb4ec820980e0dc82bd28352a6cc76057
Fil Pizlo <pizlo@mac.com>
2025-09-22 11:49:29 -0700

    Add procps to the corpus

--- build_procps.sh
+++ build_procps.sh
@@ -28,20 +28,9 @@
 set -e
 set -x
 
-# This is the set of programs that I've confirmed build fine with glibc, but haven't confirmed that
-# they build fine with musl. If we confirm that they build with musl we should move the build command
-# to build_all.sh.
-
-./build_gmp.sh
-./build_attr.sh
-./build_gettext.sh
-./build_grep.sh
-./build_elfutils.sh
-./build_check.sh
-./build_diffutils.sh
-./build_bison.sh
-./build_libpipeline.sh
-./build_libcap.sh
-./build_procps.sh
-./build_make.sh
+cd projects/procps-ng-4.0.4
+extract_source
+PATH=$PWD/../../../pizfix/bin:$PATH CC=$PWD/../../../build/bin/clang ./configure --prefix=$PWD/../../../pizfix --disable-static
+make -j $NCPU
+make -j $NCPU install
 
