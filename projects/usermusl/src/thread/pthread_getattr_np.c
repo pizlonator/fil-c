750ad46a8a019502df57c1df8f176548d4dcf03f
Fil Pizlo <pizlo@mac.com>
2025-11-14 12:37:45 -0800

    Make the Ruby build work with musl

--- projects/usermusl/src/thread/pthread_getattr_np.c
+++ projects/usermusl/src/thread/pthread_getattr_np.c
@@ -8,17 +8,7 @@ int pthread_getattr_np(pthread_t t, pthread_attr_t *a)
 	*a = (pthread_attr_t){0};
 	a->_a_detach = t->detach_state>=DT_DETACHED;
 	a->_a_guardsize = t->guard_size;
-	if (t->stack) {
-		a->_a_stackaddr = (uintptr_t)t->stack;
-		a->_a_stacksize = t->stack_size;
-	} else {
-		char *p = (void *)libc.auxv;
-		size_t l = PAGE_SIZE;
-		p += -(uintptr_t)p & PAGE_SIZE-1;
-		a->_a_stackaddr = (uintptr_t)p;
-		while (mremap(p-l-PAGE_SIZE, PAGE_SIZE, 2*PAGE_SIZE, 0)==MAP_FAILED && errno==ENOMEM)
-			l += PAGE_SIZE;
-		a->_a_stacksize = l;
-	}
+        a->_a_stackaddr = 0;
+        a->_a_stacksize = 0;
 	return 0;
 }
