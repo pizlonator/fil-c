c173bfaf81665af61757fe8c73753e9d0ba54cb3
Fil Pizlo <pizlo@mac.com>
2026-01-04 00:51:53 +0100

    Fix usermusl's posix_spawn_file_actions_addclose, which turned out to not return EBADF in the right situations. For some reason I am learning about this now from the m4 test suite that the open support is fixed.

--- projects/usermusl/src/process/posix_spawn_file_actions_addclose.c
+++ projects/usermusl/src/process/posix_spawn_file_actions_addclose.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <errno.h>
 #include "fdop.h"
+#include <pizlonated_syscalls.h>
+#include <sys/resource.h>
 
 int posix_spawn_file_actions_addclose(posix_spawn_file_actions_t *fa, int fd)
 {
-	if (fd < 0) return EBADF;
+	struct rlimit nofile_limit;
+	ZASSERT(!zsys_getrlimit(RLIMIT_NOFILE, &nofile_limit));
+	if (fd < 0 || fd >= nofile_limit.rlim_cur) return EBADF;
 	struct fdop *op = malloc(sizeof *op);
 	if (!op) return ENOMEM;
 	op->cmd = FDOP_CLOSE;
