1d486cb411590459aecc189b2a55d35d8195f82f
Saam Barati <saam@hey.com>
2026-01-19 10:25:39 -0800

    Add libgcrypt to the pizlix build.

--- pizlix/build_postlc5_sub1_packaging.sh
+++ pizlix/build_postlc5_sub1_packaging.sh
@@ -18,4 +18,5 @@ rm -vf projects/*/pizlonated-*.tar.gz
 ./package-source.sh projects/glib-networking-2.80.0 pizlonated-glib-networking
 ./package-source.sh projects/libsoup-3.4.4 pizlonated-libsoup
 ./package-source.sh projects/libgpg-error-1.50 pizlonated-libgpg-error
+./package-source.sh projects/libgcrypt-1.11.0 pizlonated-libgcrypt
 
